77e26fddbec82f42ce2f1f7011f4c21664c7be91
Jasmine Taylor <83115753+j4sm1ne96@users.noreply.github.com>
2022-01-16 17:07:26 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const timeContainer = document.getElementById("time-container");
+const BIRTH_DAY = "04-05-1996";
+const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
+const intlNumberFormatter = new Intl.NumberFormat("en-US");
+
+setInterval(() => {
+    const now = new Date();
+    const difference = Math.floor(
+        (now.getTime() - BIRTH_DAY_DATE.getTime())/1000
+    );
+    timeContainer.innerText = intlNumberFormatter.format(difference);
+}, 1000);
